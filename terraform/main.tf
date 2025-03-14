2c607550415ef18bbf8d27c0c9b8ce88ea68be61
Brian Pando <unasbrian@gmail.com>
2025-03-14 11:23:07 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "digitalocean_droplet" "flask_server" {
   size   = "s-1vcpu-1gb"  # 1GB RAM, 1 vCPU
   image  = "ubuntu-22-04-x64"
 
-  ssh_keys = var.ssh_key_fingerprints
+  ssh_keys = var.ssh_keys
 
   user_data = file("${path.module}/install.sh")
 
